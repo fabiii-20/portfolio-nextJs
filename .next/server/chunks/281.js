990c4bfec0aef7a2377389b13e6f3348d5b46b5a
faznaa <faznaaharees@gmail.com>
2024-10-09 13:17:40 +0530

    added more projects

---
+++ .next/server/chunks/281.js
@@ -0,0 +1,235 @@
+"use strict";
+exports.id = 281;
+exports.ids = [281];
+exports.modules = {
+
+/***/ 8281:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "n": () => (/* binding */ projectsData)
+/* harmony export */ });
+/* harmony import */ var uuid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6555);
+/* harmony import */ var react_icons_fi__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2750);
+/* harmony import */ var react_icons_fi__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_icons_fi__WEBPACK_IMPORTED_MODULE_1__);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([uuid__WEBPACK_IMPORTED_MODULE_0__]);
+uuid__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+
+const projectsData = [
+    {
+        id: 1,
+        title: "Klashra",
+        url: "",
+        subtitle: "A tour booking platform for Dubai based travel company",
+        category: "Web Application",
+        img: "/images/web-project1.png",
+        ProjectHeader: {
+            title: "Tour planning platform",
+            publishDate: "July , 2023",
+            tags: "UI / Frontend"
+        },
+        ProjectImages: [
+            {
+                id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                title: "image",
+                img: "/images/ui-project1.png"
+            },
+            {
+                id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                title: "image",
+                img: "/images/web-project1.png"
+            },
+            {
+                id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                title: "image",
+                img: "/images/web-project1-1.png"
+            }
+        ],
+        ProjectInfo: {
+            ClientHeading: "Client",
+            CompanyInfo: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "Klashra Tours and Travels"
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "UI Designer & Frontend Developer"
+                }
+            ],
+            ObjectivesHeading: "Objective",
+            ObjectivesDetails: "To create an engaging and user-friendly online platform tailored for travelers worldwide, with a specific focus on the captivating destinations nestled within Dubai.",
+            Technologies: [
+                {
+                    title: "Tools & Technologies",
+                    techs: [
+                        "HTML",
+                        "CSS",
+                        "JavaScript",
+                        "React js",
+                        "TailwindCSS",
+                        "Figma"
+                    ]
+                }
+            ],
+            DeployedUrlHeading: "Deployed site",
+            DeployedUrlInfo: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "Deployed site",
+                    url: "https://klashra-test-domain-1.netlify.app/"
+                }
+            ],
+            FigmaUrlHeading: "UI Design",
+            FigmaUrlInfo: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "UI Design",
+                    url: "https://www.figma.com/file/uAZSZjAf5JOFK8ucaT8bzu/klashra?type=design&node-id=58%3A2&mode=design&t=dDP9OG4rB0FaVD3F-1"
+                }
+            ],
+            ProjectDetailsHeading: "Challenge",
+            ProjectDetails: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "As a Frontend Developer and UI/UX Designer at Klashra, my mission was to create an engaging and user-friendly online platform tailored for travelers worldwide, with a specific focus on the captivating destinations nestled within Dubai."
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: 'Utilized Figma to create wireframes, mockups, and interactive prototypes to visually communicate design concepts effectively. Leveraged the "smart animate" feature in Figma to craft dynamic and engaging prototypes.'
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "Applied user-centered design principles to ensure the platform`s interfaces were intuitive and visually appealing. Focused on creating a seamless user experience to enhance engagement."
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "Leveraged a tech stack that included React.js, HTML, Bootstrap CSS, and JavaScript to implement responsive and interactive user interfaces. Ensured that the platform worked seamlessly across various devices and screen sizes."
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "Utilized React hooks to manage state and functional components effectively, streamlining the development process. Implemented custom card components to showcase travel information and destinations and Integrated the Stripe payment gateway to facilitate secure and efficient payment processing within the platform. Ensured a smooth payment experience for users. Deployed the react app netlify"
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        title: "QIB Logitics",
+        subtitle: "A landing page for a Canada based Import/Export company",
+        url: "",
+        category: "Branding",
+        img: "/images/web-project2.png",
+        ProjectHeader: {
+            title: "QIB Logistics",
+            publishDate: "Sep, 2021",
+            tags: "UI/Frontend"
+        },
+        ProjectImages: [
+            {
+                id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                title: "Phoenix Digital Agency",
+                img: "/images/web-project2.png"
+            },
+            {
+                id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                title: "Phoenix Digital Agency",
+                img: "/images/ui-project2.png"
+            },
+            {
+                id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                title: "Phoenix Digital Agency",
+                img: "/images/web-project2-1.png"
+            }
+        ],
+        ProjectInfo: {
+            ClientHeading: "About Client",
+            CompanyInfo: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "QIB Logistics"
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "UI Designer & Frontend Developer"
+                }
+            ],
+            ObjectivesHeading: "Objective",
+            ObjectivesDetails: "The task was to craft a captivating landing page for the company. The objective was to align the design seamlessly with the brand book provided by the organization.",
+            Technologies: [
+                {
+                    title: "Tools & Technologies",
+                    techs: [
+                        "HTML",
+                        "CSS",
+                        "JavaScript",
+                        "React Js",
+                        "BoootstrapCSS",
+                        "Figma"
+                    ]
+                }
+            ],
+            DeployedUrlHeading: "Deployed site",
+            DeployedUrlInfo: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "Deployed site",
+                    url: "https://darling-empanada-84e43d.netlify.app/"
+                }
+            ],
+            FigmaUrlHeading: "UI Design",
+            FigmaUrlInfo: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    title: "UI Design",
+                    url: "https://www.figma.com/proto/31amN5fsPovOG3Gmximryz/QIB?type=design&node-id=1-2&t=VTAO213Xccm2cH82-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2"
+                }
+            ],
+            ProjectDetailsHeading: "Challenge",
+            ProjectDetails: [
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "In my capacity as a Frontend Developer and UI/UX Designer at QIB Logistics, I undertook the pivotal task of crafting a captivating landing page for the company. The objective was to align the design seamlessly with the brand book provided by the organization."
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "Design Consistency with Brand Book: Ensured strict adherence to the company`s brand book guidelines to maintain a cohesive and consistent visual identity. This approach was fundamental in preserving the brand`s image."
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: "UI Design and Prototyping in Figma: Leveraged Figma to meticulously design the user interface (UI) for the landing page. The design process involved creating aesthetically pleasing and user-friendly elements that resonated with the brand`s ethos."
+                },
+                {
+                    id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v4)(),
+                    details: " Frontend Development Expertise: Employed a robust tech stack consisting of React.js, Bootstrap CSS, and TypeScript for the frontend development. This stack provided the foundation for a responsive, high-performance, and interactive landing page."
+                }
+            ]
+        }
+    },
+    {
+        id: 3,
+        title: "Nexwave",
+        subtitle: "Website Services for a Digital Agency",
+        category: "Web Application",
+        img: "/images/nexwave.png",
+        url: "https://main--glistening-baklava-a3e7a8.netlify.app/"
+    },
+    {
+        id: 4,
+        title: "RestoMenu",
+        subtitle: "A menu display application",
+        category: "Web Application",
+        img: "/images/1.png",
+        url: "https://beautiful-gaufre-fffa52.netlify.app/"
+    }
+];
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ })
+
+};
+;
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -64,11 +64,12 @@ export default function Skills() {
 
         <h2 className="text-3xl sm:text-6xl font-bold tracking-tight text-black dark:text-white  pt-6 ">Skills</h2>
        {/* <FadeIn> */}
-            {skills.map((skill,index)=><div className='flex  w-full flex-row justify-between sm:gap-x-6 items-center lg:items-start my-10'>
+            {skills.map((skill,index)=><div key={`skill-${index}`} className='flex  w-full flex-row justify-between sm:gap-x-6 items-center lg:items-start my-10'>
                 <div className=' flex flex-col justify-between items-start'>
                     <h1 className='text-lg sm:text-2xl dark:text-gray-400 text-gray-600 hover:text-gray-900 transition-all duration-300 cursor-pointer  tracking-tight font-semibold mb-2 '>{skill.title}</h1>
                     <div className='flex flex-wrap gap-x-2 gap-y-4'>                    {skill.skills.map((skill,sIndex)=><div 
                     // style={{background:randomPastelHex()}}
+                    key={`skillsub-${sIndex}`}
                      className={`text-md sm:text-lg tracking-tight bg-blue-100 hover:bg-blue-200 transition-all duration-300 cursor-pointer text-black rounded-xl px-4 py-2 border border-black  `}> 
 
                      {skill}
